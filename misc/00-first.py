31e7fdbdca1183ed9ca8ebf5239ef8382038c98b
dlnp2 <hideta7.dev@gmail.com>
2017-03-31 01:08:11 +0900

    isolate and delete module-local variables

--- misc/00-first.py
+++ misc/00-first.py
@@ -1,16 +1,19 @@
-from datetime import datetime
+from __future__ import print_function
 import os
-loc = os.path.abspath(os.path.join(os.path.dirname(__file__), __file__))
 
-# check conda environment
-import subprocess
-envs = subprocess.check_output(['conda-env', 'list']).splitlines()
-env = list(filter(lambda s: '*' in str(s), envs))[0]
-name = env.decode('utf-8').split('*')[0].strip()
-
-print("\nloading the default libraries for {} environment specified in\n{}".format(name, loc))
+def conda_environment_name():
+    """ check conda environment """
+    import subprocess
+    loc = os.path.abspath(os.path.join(os.path.dirname(__file__), __file__))
+    envs = subprocess.check_output(['conda-env', 'list']).splitlines()
+    env = list(filter(lambda s: '*' in str(s), envs))[0]
+    name = env.decode('utf-8').split('*')[0].strip()
+    print("\nloading the default libraries for {} environment specified in\n{}".format(name, loc))
+    return name
 
 # default libraries
+name = conda_environment_name()
+from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -25,5 +28,9 @@ else:
     import pandas as pd
     from pandas import Series, DataFrame
     pd.options.display.max_columns = 40
+
+# remove the module-local variables to avoid polluting the namespace
+del name
+del conda_environment_name
 
 print('done.')
