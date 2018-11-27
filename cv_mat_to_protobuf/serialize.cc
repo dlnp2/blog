50a6a9469e959dc1f4e0af1e3a6113d6b5e717d8
dlnp2 <hideta7.dev@gmail.com>
2018-11-27 14:56:40 +0900

    remove unnecessary lines

--- cv_mat_to_protobuf/serialize.cc
+++ cv_mat_to_protobuf/serialize.cc
@@ -7,16 +7,6 @@
 
 using image::Image;
 
-typedef unsigned char byte;
-
-byte *matToBytes(cv::Mat image) {
-  // Copy cv::Mat.data to unsigned char array
-  int size = image.total() * image.elemSize();
-  byte *bytes = new byte[size];
-  std::memcpy(bytes, image.data, size * sizeof(byte));
-  return bytes;
-}
-
 int main(int argc, char** argv) {
   // Read image with alpha channel
   cv::Mat img = cv::imread(argv[1], cv::IMREAD_UNCHANGED);
@@ -26,23 +16,23 @@ int main(int argc, char** argv) {
   int height = img.rows;
   int channel = img.channels();
   int size = width * height * channel;
-  byte *data = matToBytes(img);
-  std::string data_str(reinterpret_cast<char const*>(data), size);
+  std::string data_str(reinterpret_cast<char const*>(img.data), size);
 
   std::cout << "Original image width  : " << width << std::endl;
   std::cout << "Original image height : " << height << std::endl;
   std::cout << "Original image channel: " << channel << std::endl;
 
-  // Store into protobuf and serialize
+  // Store into protobuf
   Image image;
   image.set_width(width);
   image.set_height(height);
   image.set_channel(channel);
   image.set_data(data_str);
 
+  // Serialize to .pb
   std::fstream outpb("out.pb", std::ios::out | std::ios::trunc | std::ios::binary);
   if (!image.SerializeToOstream(&outpb)) {
-    std::cerr << "Failed to serialize to .pb" << std::endl;
+    std::cerr << "Failed to serialize." << std::endl;
     return -1;
   } else {
     std::cout << "Data dumped to out.pb." << std::endl;
