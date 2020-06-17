45293bda5ed26d0b49e74d4fddab970d8ef91b3c
dlnp2 <hideta7.dev@gmail.com>
2020-06-18 01:28:42 +0900

    update using atmark macro

--- monte_carlo_statistical_calculation/2.6.jl
+++ monte_carlo_statistical_calculation/2.6.jl
@@ -1,4 +1,6 @@
 function gen_weibull_variables(a, b, n_samples)
-    x = b .* (.- log.(1 - rand(n_samples))) .^ (1 / a)
+    u = rand(n_samples)
+    x = similar(u)
+    @. x = b * (-log(1 - u)) ^ (1 / a)
 end
 println(gen_weibull_variables(1, 1, 10))
