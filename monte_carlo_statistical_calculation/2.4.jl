ae1b065630895548451c96893ab577bf02f459ef
dlnp2 <hideta7.dev@gmail.com>
2020-06-18 00:52:33 +0900

    update using vectorized expression

--- monte_carlo_statistical_calculation/2.4.jl
+++ monte_carlo_statistical_calculation/2.4.jl
@@ -1,7 +1,4 @@
 function gen_pareto_variables(a, b, n_samples)
-    x = zeros(n_samples)
-    for i in 1:n_samples
-        x[i] = b * (1 - rand()) ^ (-1 / a)
-    end
-    x
+    x = b .* (1 .- rand(n_samples)) .^ (-1 / a)
 end
+println(gen_pareto_variables(1, 1, 10))
